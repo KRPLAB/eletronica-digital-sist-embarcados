9ea36ea5baefa245cce874dbed3ae82dae7f6a38
KRPLAB <kauan.r.paulino@gmail.com>
2025-09-27 02:22:49 -0300

    :sparkles: feat (console analog read)

--- main/leitor_ldr.c
+++ main/leitor_ldr.c
@@ -2,8 +2,7 @@
 #include "drivers/adc.h"
 #include <stdio.h>
 
-void app_main(void)
-{
+void app_main(void){
 	//--- 1. CONFIGURACAO DO ADC ---
 	// Configura a resolucao do ADC1 para 12 bits (0-4095)
 	adc1_config_width(ADC_WIDTH_BIT_12);
@@ -17,6 +16,15 @@ void app_main(void)
 	// Configura o GPIO 23 como saída
 	gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
 
+	//--- LOOP PRINCIPAL ---
+	while(1){
+		// 1. ler o valor bruto do ADC do canal 6 (GPIO34)
+		int valod adc = adc1_get_raw(ADC_CHANNEL_6);
 
-	while(1);
+		// 2. Imprimir o valor no console (monitor seial)
+		printf("Valor do LDR: %d\n", valor_adc);
+
+		// 3. Aguardar 1 segundo antes da proxima leitura
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
 }
